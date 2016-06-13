f1e3e5ba2ff5432d8621170ba9ca4b73e41686e2
zhoucong <zhoucong@zhoucong-PC>
2016-06-13 12:59:46 +0800

    6.13

--- src/productAndConsume/StorageWithBlockQueue.java
+++ src/productAndConsume/StorageWithBlockQueue.java
@@ -6,7 +6,7 @@ import java.util.concurrent.BlockingQueue;
 public class StorageWithBlockQueue implements StorageInterface{
 	private BlockingQueue<Product> queue = new ArrayBlockingQueue<Product>(10);//必须声明阻塞队列大小
 	
-	//BlockingQueue中的offer，如果队列慢了直接return false。
+	//BlockingQueue中的offer，如果队列满了直接return false。
 	//put 如果队列满了，将该线程加入notFull的阻塞队列，队列不满时会唤醒该线程
 	public void product(Product p) throws InterruptedException{      
 		queue.put(p);            //不可以用offer，只能用put。必须处理InterruptedException
